d501b748b31985f5bb7271a21c1b4ee70022d503
WalaaRababa <121346069+WaalaaKamel@users.noreply.github.com>
2024-05-10 00:05:52 +0300

    start working on UI

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'/home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'about',component:AboutComponent},
+    {path:'**', component:NotfoundComponent}
+];
